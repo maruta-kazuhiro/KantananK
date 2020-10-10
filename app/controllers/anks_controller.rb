31c3b09469b40c7ae7cfce1d7504d63cbd34f917
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-10-10 23:54:36 +0900

    ユーザー登録機能

--- app/controllers/anks_controller.rb
+++ app/controllers/anks_controller.rb
@@ -1,7 +1,16 @@
 class AnksController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @anks = Ank.all
+  end
+
+  private
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
