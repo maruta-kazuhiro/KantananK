2281d7bf154468e9d30f387e1899d386086a5b02
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-10-11 16:06:32 +0900

    削除機能の実装

--- app/controllers/anks_controller.rb
+++ app/controllers/anks_controller.rb
@@ -1,12 +1,29 @@
 class AnksController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @anks = Ank.all
   end
 
+  def new
+    @ank = Ank.new
+  end
+
+  def create
+    Ank.create!(ank_params)
+  end
+
+  def destroy
+    ank = Ank.find(params[:id])
+    ank.destroy
+  end
+
   private
-  
+
+  def ank_params
+    params.require(:ank).permit(:image, :question, :answer1, :answer2, :answer3)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
--- app/models/ank.rb
+++ app/models/ank.rb
@@ -1,2 +1,11 @@
 class Ank < ApplicationRecord
+  belongs_to :user, optional: true
+
+  with_options presence: true do
+    validates :question
+    validates :answer1
+    validates :answer2
+    validates :answer3
+  end
+  
 end
