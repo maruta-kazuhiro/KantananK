a0491d8cf10f6268502dcd9ff40cf926c10e09bc
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-10-21 21:57:14 +0900

    ビューの一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :anks, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
 
   validates :nickname, presence: true
